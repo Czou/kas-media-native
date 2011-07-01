a16598eecedaf35c8d5aabe69cbd6936072dfdb4
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-07-01 09:59:43 +0000

    Avoid compliation of ffmpeg shared library that is not used

--- jni/ffmpeg-0.7-rc1/Android.mk
+++ jni/ffmpeg-0.7-rc1/Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_STATIC_LIBRARIES := libavformat libavcodec libavutil libpostproc libswscale
-LOCAL_MODULE := ffmpeg
-include $(BUILD_SHARED_LIBRARY)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
