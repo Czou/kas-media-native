089c2e7e69fbe9b100760dfea886fff376fd8159
Raúl Benítez <rbenitez@gsyc.es>
2011-10-06 17:36:32 +0200

    Add new method for take port

--- src/com/kurento/kas/media/ports/MediaPortManager.java
+++ src/com/kurento/kas/media/ports/MediaPortManager.java
@@ -3,9 +3,11 @@ package com.kurento.kas.media.ports;
 public class MediaPortManager {
 
 	public static native int takeAudioLocalPort();
+	public static native int takeAudioLocalPort(int audioPort);
 	public static native void releaseAudioLocalPort();
 	
 	public static native int takeVideoLocalPort();
+	public static native int takeVideoLocalPort(int videoPort);
 	public static native void releaseVideoLocalPort();
 	
 	static {
